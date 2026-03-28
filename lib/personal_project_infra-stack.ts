93e00f43719f8b1eac92327be1688e966c55d2af
Addison Kauzer <addisonjkauzer@gmail.com>
2026-03-28 09:59:09 -0700

    Add S3 and CloudFront for portfolio dashboard and recordings CDN

--- lib/personal_project_infra-stack.ts
+++ lib/personal_project_infra-stack.ts
@@ -5,6 +5,9 @@ import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 import * as path from 'path';
 
@@ -14,6 +17,13 @@ export class PersonalProjectInfraStack extends cdk.Stack {
 
     const recordingsBucket = new s3.Bucket(this, 'RecordingsBucket', {
       bucketName: 'linkedin-puzzle-recordings',
+      versioned: true,
+      cors: [{
+        allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.HEAD],
+        allowedOrigins: ['*'],
+        allowedHeaders: ['*'],
+        maxAge: 3000,
+      }],
     });
 
     const visualizerLambda = new lambda.DockerImageFunction(this, 'LinkedInPuzzleVisualizer', {
@@ -39,7 +49,62 @@ export class PersonalProjectInfraStack extends cdk.Stack {
       resources: [`arn:aws:ssm:${this.region}:${this.account}:parameter/linkedin-puzzle-solvers/anthropic-api-key`],
     }));
 
-    recordingsBucket.grantPut(visualizerLambda);
+    recordingsBucket.grantReadWrite(visualizerLambda);
+
+    const distribution = new cloudfront.Distribution(this, 'PuzzleDistribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(recordingsBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+      },
+      additionalBehaviors: {
+        'recordings/*': {
+          origin: origins.S3BucketOrigin.withOriginAccessControl(recordingsBucket),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        },
+      },
+      comment: 'LinkedIn Puzzle Recordings',
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDomain', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'CloudFront URL for puzzle recordings and metadata',
+    });
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: 'linkedin-puzzle-dashboard',
+    });
+
+    const websiteDistribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [{
+        httpStatus: 403,
+        responseHttpStatus: 200,
+        responsePagePath: '/index.html',
+      }],
+      comment: 'LinkedIn Puzzle Dashboard',
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../PersonalPortfolio/dist'))],
+      destinationBucket: websiteBucket,
+      distribution: websiteDistribution,
+      distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: `https://${websiteDistribution.distributionDomainName}`,
+      description: 'URL for the puzzle dashboard website',
+    });
 
     const dailyRule = new events.Rule(this, 'DailyVisualizationRule', {
       ruleName: 'LinkedInPuzzleVisualizerDaily',
